29d8c3730f2b47ff97dd16cc3d0189194062fc05
Dhivyaa <dhivyaa.ravi@zohocorp.com>
2022-11-17 00:54:41 +0530

    Completed dashboard and login, signup backend

--- app/app.py
+++ app/app.py
@@ -1,60 +1,136 @@
-from flask import Flask, render_template, redirect, request, session, flash
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask import Flask, render_template, url_for, request, redirect, session, make_response
+import sqlite3 as sql
+from functools import wraps
+import re
+import ibm_db
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from datetime import datetime, timedelta
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=ea286ace-86c7-4d5b-8580-3fbfa46b1c66.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31505;SECURITY=SSL;SSLServerCertificate=ssl.crt;UID=qxn78437;PWD=whNl99ZgMIttkZ80", '', '')
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gibsqbxqelvght:bd2af40fd3a7372a4da33e7090e7606349d24f661df710450123851701b84fe8@ec2-44-209-186-51.compute-1.amazonaws.com:5432/dcrlivg5r4ajd1'
-app.config['SECRET_KEY'] = 'mysecret'
+app.secret_key = 'jackiechan'
 
-engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
-db = scoped_session(sessionmaker(bind = engine))
+
+def rewrite(url):
+    view_func, view_args = app.create_url_adapter(request).match(url)
+    return app.view_functions[view_func](**view_args)
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "id" not in session:
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/')
-def index():
-  return render_template('index.html')
+def root():
+    return render_template('login.html')
 
 
-@app.route('/login')
+@app.route('/user/<id>')
+@login_required
+def user_info(id):
+    with sql.connect('inventorymanagement.db') as con:
+        con.row_factory = sql.Row
+        cur = con.cursor()
+        cur.execute(f'SELECT * FROM users WHERE email="{id}"')
+        user = cur.fetchall()
+    return render_template("user_info.html", user=user[0])
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-  session['user'] = 'mathan'
-  return render_template('login.html')
+    global userid
+    msg = ''
 
-@app.route('/register')
-def register():
-  return render_template('register.html')
+    if request.method == 'POST':
+        un = request.form['username']
+        pd = request.form['password_1']
+        print(un, pd)
+        sql = "SELECT * FROM users WHERE email =? AND password=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, un)
+        ibm_db.bind_param(stmt, 2, pd)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['EMAIL']
+            userid = account['EMAIL']
+            session['username'] = account['USERNAME']
+            msg = 'Logged in successfully !'
 
-@app.route('/wallet')
-def wallet():
-  wallet = db.execute("SELECT balance, wallet_limit FROM wallet WHERE username = :username",
-              {
-                "username": session['user']
-              }).fetchone()
+            return rewrite('/dashboard')
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg=msg)
 
-  return render_template('wallet.html', wallet=wallet)
-  
 
-@app.route('/wallet/update-limit', methods=["GET", "POST"])
-def updateWalletLimit():
-  if request.method == "GET":
-    return render_template("update-wallet.html")
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    mg = ''
+    if request.method == "POST":
+        username = request.form['username']
+        email = request.form['email']
+        pw = request.form['password']
+        sql = 'SELECT * FROM users WHERE email =?'
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, email)
+        ibm_db.execute(stmt)
+        acnt = ibm_db.fetch_assoc(stmt)
+        print(acnt)
 
-  limit =  float(request.form['limit'])
+        if acnt:
+            mg = 'Account already exits!!'
 
-  if limit < 0:
-    return "<h1>Invalid limit</h1>"
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            mg = 'Please enter the avalid email address'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            ms = 'name must contain only character and number'
+        else:
+            insert_sql = 'INSERT INTO users (USERNAME,FIRSTNAME,LASTNAME,EMAIL,PASSWORD) VALUES (?,?,?,?,?)'
+            pstmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(pstmt, 1, username)
+            ibm_db.bind_param(pstmt, 2, "firstname")
+            ibm_db.bind_param(pstmt, 3, "lastname")
+            # ibm_db.bind_param(pstmt,4,"123456789")
+            ibm_db.bind_param(pstmt, 4, email)
+            ibm_db.bind_param(pstmt, 5, pw)
+            print(pstmt)
+            ibm_db.execute(pstmt)
+            mg = 'You have successfully registered click login!'
+            message = Mail(
+                from_email=os.environ.get('MAIL_DEFAULT_SENDER'),
+                to_emails=email,
+                subject='New SignUp',
+                html_content='<p>Hello, Your Registration was successfull. <br><br> Thank you for choosing us.</p>')
 
-  db.execute("UPDATE wallet SET wallet_limit = :limit WHERE username = :username",
-              {
-                "limit" : limit,
-                "username" : session['user']
-              })
-  db.commit()
+            sg = SendGridAPIClient(
+                api_key=os.environ.get('SENDGRID_API_KEY'))
 
-  flash('wallet updated succesfully', 'success')
-  return redirect("/wallet")
+            response = sg.send(message)
+            print(response.status_code, response.body)
+            return render_template("login.html", meg=mg)
 
-@app.route('/logout')
-def logout():
-  session.clear()
-  return redirect('/')
+    elif request.method == 'POST':
+        msg = "fill out the form first!"
+    return render_template("signup.html", meg=mg)
+
+
+@app.route('/dashboard', methods=['POST', 'GET'])
+@login_required
+def dashBoard():
+    return render_template('dashboard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# ALTER TABLE stocks ALTER COLUMN ID SET GENERATED BY DEFAULT AS IDENTITY
