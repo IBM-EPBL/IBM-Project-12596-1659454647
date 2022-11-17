cf5deebc49f3f09533a79be4b76acc27d61bee5e
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2022-11-18 00:12:19 +0530

    updated adding expenses

--- app/app.py
+++ app/app.py
@@ -22,7 +22,7 @@ def rewrite(url):
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if "id" not in session:
+        if "username" not in session:
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
@@ -34,12 +34,12 @@ def root():
 
 
 @app.route('/user/<id>')
-@login_required
+# @login_required
 def user_info(id):
     with sql.connect('inventorymanagement.db') as con:
         con.row_factory = sql.Row
         cur = con.cursor()
-        cur.execute(f'SELECT * FROM users WHERE email="{id}"')
+        cur.execute(f'SELECT * FROM users')
         user = cur.fetchall()
     return render_template("user_info.html", user=user[0])
 
@@ -53,7 +53,7 @@ def login():
         un = request.form['username']
         pd = request.form['password_1']
         print(un, pd)
-        sql = "SELECT * FROM users WHERE email =? AND password=?"
+        sql = "SELECT * FROM users WHERE username =? AND password=?"
         stmt = ibm_db.prepare(conn, sql)
         ibm_db.bind_param(stmt, 1, un)
         ibm_db.bind_param(stmt, 2, pd)
@@ -61,16 +61,14 @@ def login():
         account = ibm_db.fetch_assoc(stmt)
         print(account)
         if account:
-            session['loggedin'] = True
-            session['id'] = account['EMAIL']
-            userid = account['EMAIL']
             session['username'] = account['USERNAME']
-            msg = 'Logged in successfully !'
-
-            return rewrite('/dashboard')
+            session['email'] = account['EMAIL']
+            return redirect('/dashboard')
         else:
             msg = 'Incorrect username / password !'
-    return render_template('login.html', msg=msg)
+        return render_template('login.html', msg=msg)
+    else:
+        return render_template('login.html')
 
 
 @app.route('/signup', methods=['POST', 'GET'])
@@ -80,53 +78,34 @@ def signup():
         username = request.form['username']
         email = request.form['email']
         pw = request.form['password']
-        sql = 'SELECT * FROM users WHERE email =?'
+        sql = 'SELECT * FROM users WHERE username =?'
         stmt = ibm_db.prepare(conn, sql)
-        ibm_db.bind_param(stmt, 1, email)
+        ibm_db.bind_param(stmt, 1, username)
         ibm_db.execute(stmt)
         acnt = ibm_db.fetch_assoc(stmt)
-        print(acnt)
-
         if acnt:
             mg = 'Account already exits!!'
-
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            mg = 'Please enter the avalid email address'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            ms = 'name must contain only character and number'
+            return render_template('signup.html', meg=mg)
         else:
-            insert_sql = 'INSERT INTO users (USERNAME,FIRSTNAME,LASTNAME,EMAIL,PASSWORD) VALUES (?,?,?,?,?)'
+            insert_sql = 'INSERT INTO users (USERNAME,EMAIL,PASSWORD) VALUES (?,?,?)'
             pstmt = ibm_db.prepare(conn, insert_sql)
             ibm_db.bind_param(pstmt, 1, username)
-            ibm_db.bind_param(pstmt, 2, "firstname")
-            ibm_db.bind_param(pstmt, 3, "lastname")
-            # ibm_db.bind_param(pstmt,4,"123456789")
-            ibm_db.bind_param(pstmt, 4, email)
-            ibm_db.bind_param(pstmt, 5, pw)
-            print(pstmt)
+            ibm_db.bind_param(pstmt, 2, email)
+            ibm_db.bind_param(pstmt, 3, pw)
             ibm_db.execute(pstmt)
-            mg = 'You have successfully registered click login!'
-            message = Mail(
-                from_email=os.environ.get('MAIL_DEFAULT_SENDER'),
-                to_emails=email,
-                subject='New SignUp',
-                html_content='<p>Hello, Your Registration was successfull. <br><br> Thank you for choosing us.</p>')
 
-            sg = SendGridAPIClient(
-                api_key=os.environ.get('SENDGRID_API_KEY'))
+            session['username'] = username
+            session['email'] = email
 
-            response = sg.send(message)
-            print(response.status_code, response.body)
-            return render_template("login.html", meg=mg)
+            return redirect('/dashoard')
 
-    elif request.method == 'POST':
-        msg = "fill out the form first!"
-    return render_template("signup.html", meg=mg)
+    else:
+        return render_template("signup.html", meg=mg)
 
 
 @app.route('/dashboard', methods=['POST', 'GET'])
 @login_required
-def dashBoard():
+def dashboard():
     return render_template('dashboard.html')
 
 
@@ -134,29 +113,44 @@ def dashBoard():
 @login_required
 def expense():
     if request.method == "GET":
-        sql = "SELECT * FROM expenses"
-        stmt = ibm_db.exec_immediate(conn, sql)
+        sql = "SELECT * FROM expenses WHERE user_email = ? ORDER BY datetime DESC LIMIT 10"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt, 1, session['email'])
+        ibm_db.execute(stmt)
+
         expenses = []
-        dictionary = ibm_db.fetch_assoc(stmt)
-        print(dictionary)
-        while dictionary != False:
-            expenses.append(dictionary)
-            dictionary = ibm_db.fetch_assoc(stmt)
+        record = ibm_db.fetch_assoc(stmt)
+
+        while record != False:
+            print(record)
+            expenses.append(record)
+            record = ibm_db.fetch_assoc(stmt)
 
         return render_template('expense.html', expenses=expenses)
+        
     if request.method == "POST":
         title = request.form['title']
         description = request.form['description']
         amount = request.form['Amount']
-        expenseType = request.form['exampleRadios']
-        insert_sql = 'INSERT INTO expenses (title,description,amount,expensetype) VALUES (?,?,?,?)'
+        expenseType = int(request.form['expense-type'])
+        
+        dateTime = datetime.now()
+
+        insert_sql = 'INSERT INTO expenses (user_email, title,description,amount, credit, datetime) VALUES (?, ?,?,?,?, ?)'
         pstmt = ibm_db.prepare(conn, insert_sql)
-        ibm_db.bind_param(pstmt, 1, title)
-        ibm_db.bind_param(pstmt, 2, description)
-        ibm_db.bind_param(pstmt, 3, amount)
-        ibm_db.bind_param(pstmt, 4, expenseType)
+        ibm_db.bind_param(pstmt, 1, session['email'])
+        ibm_db.bind_param(pstmt, 2, title)
+        ibm_db.bind_param(pstmt, 3, description)
+        ibm_db.bind_param(pstmt, 4, amount)
+        ibm_db.bind_param(pstmt, 5, expenseType == 1)
+        ibm_db.bind_param(pstmt, 6, dateTime)
         ibm_db.execute(pstmt)
         return redirect(url_for('expense'))
+
+@app.route('/logout')
+def logout():
+  session.clear()
+  return redirect('/login')
 
 
 if __name__ == '__main__':
