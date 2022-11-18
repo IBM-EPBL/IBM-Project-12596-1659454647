f05ef0f6b7d06c0cdda875671f3e7b34a37c25b2
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2022-11-18 15:41:08 +0530

    updated schema.sql

--- app/schema.sql
+++ app/schema.sql
@@ -1,9 +1,25 @@
 -- create users table @dhivyaa
 
+CREATE TABLE users (
+  username character(30) NOT NULL, 
+  password character(30) NOT NULL,
+  email character(30) NOT NULL
+);
+
+
 CREATE TABLE wallet (
   id BIGSERIAL PRIMARY KEY,
-  username VARCHAR(30) NOT NULL,
+  user_email CHARACTER(30) NOT NULL,
   balance FLOAT DEFAULT 0 NOT NULL,
   wallet_limit FLOAT DEFAULT 1000 NOT NULL,
-  FOREIGN KEY (username) REFERENCES users(username)
+  FOREIGN KEY (user_email) REFERENCES users(email)
+);
+
+CREATE TABLE expenses (
+  user_email CHARACTER(30) NOT NULL,
+  title character(50) NOT NULL,
+  amount float NOT NULL,
+  description character(100) NOT NULL,
+  dateTime timestamp NOT NULL,
+  credit BOOLEAN NOT NULL
 );
